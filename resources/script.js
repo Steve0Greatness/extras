d6acc083c68f595a084e8fab82d5e8e2a68a0664
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2021-06-19 20:40:47 -0700

    Update script.js

--- resources/script.js
+++ resources/script.js
@@ -1,8 +1,7 @@
 var header = document.getElementById('header')
 header.innerHTML = `<h1 style="margin: 0px;">
   <img src="https://cdn2.scratch.mit.edu/get_image/user/61820378_32x32.png"/> 
-  Hey there
-  <span style="font-style: italic;">!</span></h1>
+  Hey there<span style="font-style: italic;">!</span></h1>
   <div class="nav">
       <a target="_parent" href="/index.html">
           <button type="button">🏠 Home</button>
