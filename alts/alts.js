53f58427150cf0262b76eeb8cc43964115c8da68
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2021-07-01 19:57:27 +0000

    Fixed link

--- alts/alts.js
+++ alts/alts.js
@@ -3,6 +3,6 @@ getPages('https://api.github.com/repos/Steve0Greatness/Steve0Greatness.github.io
     .forEach((pui) => {
       var page
       page = pui.replace(/-/g, ' ');
-      if (pui != 'index') document.getElementById('index').innerHTML += '<div><a href="../Alts/' + pui + '.html" class="indexed">' + page + '</a></div>';
+      if (pui != 'index') document.getElementById('index').innerHTML += '<div><a href="./' + pui + '.html" class="indexed">' + page + '</a></div>';
     })
 });
