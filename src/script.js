ae2f94104a8aa3c2aeba3a0cb0a6fc3832d3f04c
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2021-07-03 23:04:17 +0000

    fixed errors

--- src/script.js
+++ src/script.js
@@ -1,3 +1,8 @@
+if (localStorage.getItem('theme') == 'null' || localStorage.getItem('theme') == '') {
+  localStorage.setItem('theme', 'n')
+  document.getElementById('page').className = 'n'
+}
+
 document.getElementById('header').innerHTML = `<h1 style="margin: 0px;">
   <img src="https://cdn2.scratch.mit.edu/get_image/user/61820378_32x32.png"/> 
   Hey there<span style="font-style: italic;">!</span></h1>
@@ -16,13 +21,20 @@ document.getElementById('header').innerHTML = `<h1 style="margin: 0px;">
       </a>
       <button type="button" onclick="lightSwitch()">Light Switch</button>
   </div>`;
+var url = window.location.href;
+if (url.includes('https://blog.')) {
+  url = "https://Steve0Greatnessgithubio.stevesgreatness.repl.co"
+} else {
+  url = ".."
+}
+
 var pageName = window.location.pathname.replace('/', ' ') + 'Steve0Greatness'
 pageName = pageName.replace('/', '--').replace('.html', '')
 console.log(pageName)
 document.getElementsByTagName('head')[0].innerHTML += `
-  <title>${pageName}</title>
+  <title id="title">${pageName}</title>
 	<link rel="shortcut icon" href="https://cdn2.scratch.mit.edu/get_image/user/61820378_16x16.png">
-	<link rel="stylesheet" type="text/css" href="../src/stylesheet.css">`;
+	<link rel="stylesheet" type="text/css" href="${url}/src/stylesheet.css">`;
 
 function getPages(url) {
   return new Promise((resolve, reject) => {
@@ -58,3 +70,9 @@ var theme = localStorage.getItem('theme')
 var html = document.getElementById('page')
 html.className = theme
 console.log(html, theme)
+
+this.addEventListener('keypress', event => {
+  if (event.keyCode == 13) {
+    alert('hi.')
+  }
+})
