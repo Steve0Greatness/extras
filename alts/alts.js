0da27e51ad3f3c551e356b432d696e94e845dae1
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2021-10-11 01:24:34 +0000

    did a bunch of stuff.

--- alts/alts.js
+++ alts/alts.js
@@ -17,11 +17,13 @@ fetch('alts.json')
 			console.log(data[search])
 			document.getElementById("cc").innerHTML = data[search]["desc"]
 			document.getElementById('altRating').innerHTML = data[search]["rate"]
+			document.getElementById("link").href = `https://scratch.mit.edu/users/${search}`
+			document.getElementById("link").innerHTML = "view on <img src='https://scratch.mit.edu/favicon.ico' width='16' style='margin-bottom: -2px;'>"
 			ind = document.getElementById('index')
 			l = name.length
-			ind.innerHTML = `<a href="?">🠔 index</a><br>`
+			ind.innerHTML = `<tr><a href="?">🠔 index</a></tr>`
 			for (let i = 0; i < l; i++) {
-				ind.innerHTML = ind.innerHTML + `<a href="?${name[i]}">${name[i]}</a><br>`
+				ind.innerHTML = ind.innerHTML + `<tr><td><a href="?${name[i]}">${name[i]}</a></td> <td>${data[name[i]]["rate"]}</td></tr>`
 			}
 			logo.src = `//cdn2.scratch.mit.edu/get_image/user/${data[search]["accountId"]}_32x32.png`
 			logo.alt = search
--- blog/blog.js
+++ blog/blog.js
@@ -1,28 +1,28 @@
+const newpost = "?newest&newpost"
+const search = location.search
+const ind = document.getElementById('index')
+const content = document.getElementById('content')
 fetch('blog.json')
 	.then(response => response.json())
 	.then(data => {
-		var ind = document.getElementById('index')
-		let hash = location.search
-		console.log(hash)
 		let names = Object.keys(data)
-		let content = document.getElementById('content')
 		for (let i = 0; i < names.length; i++) {
 			let x = data[names[i]]
 			console.log(i, names[i], x)
 			ind.innerHTML = ind.innerHTML + `<tr><td><a href="?${names[i]}">${x["name"]}</a></td> <td><span style="padding-left: 15px;">${x["pD"]}</span></td> </tr>`
 		}
-		if (hash != null || hash != "") {
-			if (names.indexOf(hash.slice(1)) != -1) {
-				content.innerHTML = `<h1>${data[hash.slice(1)]["name"]}</h1><h2>${data[hash.slice(1)]["pD"]}</h2><article id="article">${data[hash.slice(1)]["pC"]}</article>`
+		if (search != null || search != "" || search != newpost) {
+			if (names.indexOf(search.slice(1)) != -1) {
+				content.innerHTML = `<h1>${data[search.slice(1)]["name"]}</h1><h2>${data[search.slice(1)]["pD"]}</h2><article id="article">${data[search.slice(1)]["pC"]}</article>`
 				ind.innerHTML = ind.innerHTML + "<tr><a href='?'>- back</a></tr>"
 			}
 		}
 	});
 
+if (search == newpost) { fetch("newpost.html").then(blob => blob.text()).then(data => { content.innerHTML = `<h1>The Newest Post</h1><div style="border: solid black 1px;">${data}</div>`; if (data == "") { content.innerHTML = `<h1>null</h1>there's no current post.` } }) }
 
 if (localStorage.getItem('theme') == null || localStorage.getItem('theme') == '') {
 	localStorage.setItem('theme', 'n')
 	document.getElementById('page').className = 'n'
 }
-var pageUrl = window.location.origin
-
+var pageUrl = location.origin
