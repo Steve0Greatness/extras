141925676a5e773872a110a4e4a2020afb51e8eb
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2021-12-10 05:32:17 +0000

    Added new post and extra

--- blog/blog.js
+++ blog/blog.js
@@ -3,7 +3,7 @@ const search = location.search
 const ind = document.getElementById('index')
 const content = document.getElementById('content')
 fetch('blog.json')
-	.then(response => response.json())
+	.then(res => res.json())
 	.then(data => {
 		let names = Object.keys(data)
 		for (let i = 0; i < names.length; i++) {
@@ -11,7 +11,7 @@ fetch('blog.json')
 			console.log(i, names[i], x)
 			ind.innerHTML = ind.innerHTML + `<tr><td><a href="?${names[i]}">${x["name"]}</a></td> <td><span style="padding-left: 15px;">${x["pD"]}</span></td> </tr>`
 		}
-		if (search != null || search != "" || search != newpost) {
+		if (search != null|""|newpost) {
 			if (names.indexOf(search.slice(1)) != -1) {
 				content.innerHTML = `<h1>${data[search.slice(1)]["name"]}</h1><h2>${data[search.slice(1)]["pD"]}</h2><article id="article">${data[search.slice(1)]["pC"]}</article>`
 				ind.innerHTML = ind.innerHTML + "<tr><a href='?'>- back</a></tr>"
@@ -19,7 +19,16 @@ fetch('blog.json')
 		}
 	});
 
-if (search == newpost) { fetch("newpost.html").then(blob => blob.text()).then(data => { content.innerHTML = `<h1>The Newest Post</h1><div style="border: solid black 1px;">${data}</div>`; if (data == "") { content.innerHTML = `<h1>null</h1>there's no current post.` } }) }
+if (search == newpost) {
+	var lastModified
+	fetch("newpost.html")
+		.then(res => {
+			return res.text()
+		})
+		.then(data => {
+			content.innerHTML = `<h1>The Newest Post</h1><h2>this is where the date would go.</h2><div style="border: solid black 1px;">${data}</div>`
+		})
+}
 
 if (localStorage.getItem('theme') == null || localStorage.getItem('theme') == '') {
 	localStorage.setItem('theme', 'n')
