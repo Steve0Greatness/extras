b456861dfcf2c7f3bd9951f2de3f052a0af835a5
Steve0Greatness <75220768+Steve0Greatness@users.noreply.github.com>
2022-06-18 13:11:25 -0700

    I think this should work or something

--- src/js/script.js
+++ src/js/script.js
@@ -1,38 +1,35 @@
-var preferedThemeCheck = window.matchMedia("(prefers-color-scheme: light)"),
-	preferedTheme;
-if (preferedThemeCheck.matches) { preferedTheme = "light" } else { preferedTheme = "normal" }
-const themes = ["normal", "light"]
-if (!(themes.includes(localStorage.getItem("theme")))) { setTheme(preferedTheme) }
+window.onload = async function () {
+	var HTML = document.documentElement,
+		theme = 
+			localStorage.getItem("theme") ||
+			(
+				(
+					"matchMedia" in window &&
+					window.matchMedia("(prefers-color-scheme: light)").matches
+				)
+				? "light" : "normal"
+			),
+		THEMES = [
+			"normal",
+			"light"
+		],
+		light_switch = document.querySelector("#switch");
 
-var theme = localStorage.getItem('theme')
-function lightSwitch() {
-	if (localStorage.getItem('theme') == null) 
-		setTheme(preferedTheme)
+	function set_theme(new_theme) {
+		theme = new_theme;
+		HTML.className = new_theme;
+		localStorage.setItem("theme", new_theme);
+	}
 
-	if (theme != 'light')
-		setTheme("light");
-	else
-		setTheme("normal");
+	light_switch.addEventListener(
+		"click",
+		function next_theme() {
+			let new_theme = THEMES.indexOf(theme) + 1;
+			if (new_theme >= THEMES.length)
+				new_theme = 0;
+			set_theme(THEMES[new_theme])
+		}
+	)
 
-	theme = localStorage.getItem("theme")
-}
-
-var html = document.documentElement;
-html.className = theme;
-
-function showThenHideNote(num) {
-	var n = document.getElementById(`note${num}`);
-	n.style = "";
-	setTimeout(() => { n.style = "animation: fadeOut 3s;" }, 3000);
-	setTimeout(() => { n.style = "opacity: 0;" }, 6000);
-}
-
-function setTheme(name) {
-	localStorage.setItem('theme', name);
-	html.className = name;
-}
-
-window.onload = () => {
-	var switch_button = document.getElementById("switch");
-	switch_button.addEventListener("click", lightSwitch);
+	set_theme(theme)
 }
